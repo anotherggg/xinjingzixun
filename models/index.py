8452351d4eac537ee41715d384d98cff8b0c0cc0
崔少 <772172914@qq.com>
2020-10-09 09:13:00 +0800

    新闻详情页可以展示'评论'的测试数据

--- models/index.py
+++ models/index.py
@@ -23,6 +23,8 @@ class News(db.Model):
     user = db.relationship('User', backref=db.backref('news', lazy='dynamic'))
 
     category = db.relationship('Category', backref='news')
+    # 当前新闻的所有评论
+    comments = db.relationship("Comment", lazy="dynamic")
 
     def to_dict(self):
         """
@@ -95,3 +97,14 @@ class User(db.Model):
                                       secondary=Collection.__table__,
                                       backref=db.backref('collected_user', lazy='dynamic'),
                                       lazy='dynamic')
+
+
+class Comment(db.Model):
+    """评论"""
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)  # 评论编号
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)  # 用户id
+    news_id = db.Column(db.Integer, db.ForeignKey("news.id"), nullable=False)  # 新闻id
+    content = db.Column(db.Text, nullable=False)  # 评论内容
+    create_time = db.Column(db.DateTime, default=datetime.now)  # 记录的创建时间
+    update_time = db.Column(db.DateTime, default=datetime.now)  # 记录的更新时间
