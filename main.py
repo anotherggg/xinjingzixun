e6e329f88593e9f76640b4c9554c6860a8b633b6
崔少 <772172914@qq.com>
2020-10-12 21:12:36 +0800

    创建新蓝图，显示后台测试页面

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate,MigrateCommand
 from flask_script import Manager
 from gevent.libev.corecext import os
 
-from views import index_blu,passport_blu,user_blu,news_blu
+from views import index_blu,passport_blu,user_blu,news_blu,admin_blu
 from models import db
 from utils.common import show_top_6_news_style,show_news_status_name,show_news_status_style_name
 
@@ -18,6 +18,7 @@ app.register_blueprint(index_blu)
 app.register_blueprint(passport_blu)
 app.register_blueprint(user_blu)
 app.register_blueprint(news_blu)
+app.register_blueprint(admin_blu)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
