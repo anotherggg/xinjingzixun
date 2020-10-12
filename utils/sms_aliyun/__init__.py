17ee4106fadae61f4d6e80ec5359a69aac6f0090
崔少 <772172914@qq.com>
2020-10-12 08:47:43 +0800

    将阿里云发送短信demo,封装为函数,这样便于调用

--- utils/sms_aliyun/__init__.py
+++ utils/sms_aliyun/__init__.py
@@ -4,22 +4,27 @@
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.request import CommonRequest
 
-client = AcsClient('<accessKeyId>', '<accessSecret>', 'cn-hangzhou')
+def send_msg_to_phone(phone_num,content)
+    client = AcsClient('<accessKeyId>', '<accessSecret>', 'cn-hangzhou')
 
-request = CommonRequest()
-request.set_accept_format('json')
-request.set_domain('dysmsapi.aliyuncs.com')
-request.set_method('POST')
-request.set_protocol_type('https') # https | http
-request.set_version('2017-05-25')
-request.set_action_name('SendSms')
+    request = CommonRequest()
+    request.set_accept_format('json')
+    request.set_domain('dysmsapi.aliyuncs.com')
+    request.set_method('POST')
+    request.set_protocol_type('https') # https | http
+    request.set_version('2017-05-25')
+    request.set_action_name('SendSms')
 
-request.add_query_param('RegionId', "cn-hangzhou")
-request.add_query_param('PhoneNumbers', "18598826411")
-request.add_query_param('SignName', "dong4716138")
-request.add_query_param('TemplateCode', "SMS_167532197")
-request.add_query_param('TemplateParam', "{\"code\":\"112233\"}")
+    request.add_query_param('RegionId', "cn-hangzhou")
+    request.add_query_param('PhoneNumbers', phone_num)
+    request.add_query_param('SignName', "dong4716138")
+    request.add_query_param('TemplateCode', "SMS_167532197")
+    request.add_query_param('TemplateParam', "{\"code\":\"%s\"}" % content)
 
-response = client.do_action(request)
-# python2:  print(response)
-print(str(response, encoding = 'utf-8'))
+    response = client.do_action(request)
+    # python2:  print(response)
+    print(str(response, encoding = 'utf-8'))
+
+
+if __name__ == "__main__":
+    send_msg_to_phone("18598826411",556463)
