410eeba8007ff6b8e71e5a66d7970c10a49f77fb
崔少 <772172914@qq.com>
2020-10-13 09:24:53 +0800

    新闻分类管理实现了后端

--- views/admin.py
+++ views/admin.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 
+from models import db
+from models.index import Category
 from . import admin_blu
 
 
@@ -30,4 +32,29 @@ def news_edit():
 
 @admin_blu.route("/news_type.html")
 def news_type():
-    return render_template("admin/news_type.html")
+    news_types = db.session.query(Category).filter(Category.id != 1).all()
+    return render_template("admin/news_type.html",news_types=news_types)
+
+
+@admin_blu.route("/admin/news_type",methods=["POST"])
+def news_type_edit():
+    category_id = request.json.get("id")
+    name = request.json.get("name")
+    if category_id:
+        one_type = db.session.query(Category).filter(Category.id == category_id).first()
+        one_type.name = name
+        db.session.commit()
+        ret = {
+            "errno":0,
+            "errmsg":"修改成功"
+        }
+    else:
+        category = Category()
+        category.name = name
+        db.session.add(category)
+        db.session.commit()
+        ret = {
+            "errno": 0,
+            "errmsg": "添加成功"
+        }
+    return jsonify(ret)
