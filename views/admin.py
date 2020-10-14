c48312385d468476a05958286ccb9f9607309f7e
崔少 <772172914@qq.com>
2020-10-14 10:47:50 +0800

    审核后端视图完成

--- views/admin.py
+++ views/admin.py
@@ -39,6 +39,29 @@ def news_review_detail():
     return render_template("admin/news_review_detail.html",new=new)
 
 
+@admin_blu.route("/admin/news_review_detail/<int:news_id>",methods=["POST"])
+def news_review_detail_s(news_id):
+    # 获取当前新闻
+    news = db.session.query(News).filter(News.id == news_id).first()
+    if not news:
+        ret = {
+            "errno": 5004,
+            "errmsg": "没有找到对应的新闻"
+        }
+        return jsonify(ret)
+    action = request.json.get("action")
+    if action == "accept":
+        news.status = 0
+    else:
+        news.status = -1
+    db.session.commit()
+    ret = {
+        "errno":0,
+        "errmsg":"修改成功"
+    }
+    return jsonify(ret)
+
+
 @admin_blu.route("/news_edit.html")
 def news_edit():
     page = int(request.args.get("page",1))
